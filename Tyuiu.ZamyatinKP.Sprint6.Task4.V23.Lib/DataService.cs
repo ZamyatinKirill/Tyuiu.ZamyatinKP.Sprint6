5505f1fdbe767151e1cc2975dc6b1baa66a3ad2e
Tyuiu.ZamyatinKP@outlook.com <zamyatin_kirya@bk.ru>
2025-12-10 02:00:14 +0500

    Task4

---
+++ Tyuiu.ZamyatinKP.Sprint6.Task4.V23.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.ZamyatinKP.Sprint6.Task4.V23.Lib
+{
+    public class DataService : ISprint6Task4V23
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int length = stopValue - startValue + 1;
+            double[] results = new double[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                int x = startValue + i;
+                double functionValue;
+
+                if (2 * x - 2 == 0)
+                {
+                    functionValue = 0;
+                }
+                else
+                {
+                    functionValue = 4 - 2 * x + (2 + Math.Cos(x)) / (2 * x + 2);
+                }
+
+                results[i] = Math.Round(functionValue, 2);
+            }
+
+            return results;
+        }
+    }
+}
