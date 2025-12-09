04c60f59f15d4e7b2da89c7cc22e9670f53cb0fe
Tyuiu.ZamyatinKP@outlook.com <zamyatin_kirya@bk.ru>
2025-12-10 02:50:12 +0500

    Task7

---
+++ Tyuiu.ZamyatinKP.Sprint6.Task7.V18.Lib/DataService.cs
@@ -0,0 +1,37 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.ZamyatinKP.Sprint6.Task7.V18.Lib
+{
+    public class DataService : ISprint6Task7V18
+    {
+        public int[,] GetMatrix(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            int rows = 11;
+            int cols = 11;
+            int[,] matrix = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                string[] values = lines[i].Split(',');
+                for (int j = 0; j < cols; j++)
+                {
+                    if (int.TryParse(values[j].Trim(), out int value))
+                    {
+                        matrix[i, j] = value;
+                    }
+                }
+            }
+            int columnIndex = 8;
+
+            for (int i = 0; i < rows; i++)
+            {
+                if (matrix[i, columnIndex] != 11)
+                {
+                    matrix[i, columnIndex] = 11;
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.ZamyatinKP.Sprint6.Task7.V18.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+using Tyuiu.ZamyatinKP.Sprint6.Task7.V18.Lib;
+
+namespace Tyuiu.ZamyatinKP.Sprint6.Task7.V18.Test
+{
+    public class DataServiceTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
